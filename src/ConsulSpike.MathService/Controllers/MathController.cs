ca3f15ca1ecccc3977f92a2d3d97b5f0fb0ae9ab
Robb Schiefer <rschiefer@live.com>
2016-12-28 14:26:30 -0600

    added basic math rest service

---
+++ src/ConsulSpike.MathService/Controllers/MathController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConsulSpike.MathService.Controllers
+{
+    [Route("api/[controller]")]
+    public class MathController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Operations(string operation, double[] inputs)
+        {
+            switch (operation)
+            {
+                case "add":
+                    return inputs.Sum().ToString();
+                case "subtract":
+                    return (inputs.First() - inputs.Skip(1).Sum()).ToString();
+                case "multiply":
+                    return (inputs.Aggregate((total, next) => total * next)).ToString();
+                case "divide":
+                    return (inputs.Aggregate((total, next) => total / next)).ToString();
+                default:
+                    return "add|subtract|multiply|divide";
+            }
+        }
+    }
+}
