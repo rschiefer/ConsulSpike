19f7d9f1c8fe37628151e3601b6ed5f1a14f7242
Robb Schiefer <rschiefer@live.com>
2016-12-29 09:11:22 -0600

    web ui integrated with math rest service

--- src/ConsulSpike.AspNetCore/Controllers/HomeController.cs
+++ src/ConsulSpike.AspNetCore/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using RestSharp.Portable;
+using RestSharp.Portable.WebRequest;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,9 +20,20 @@ namespace ConsulSpike.AspNetCore.Controllers
             return View();
         }
 
-        public IActionResult Math(string operation, string inputs)
+        public async Task<IActionResult> Math(string operation, string inputs)
         {
-            var result = "test";
+            var result = string.Empty;
+
+            using (var client = new RestClient("http://localhost:22345/api/"))
+            {
+                var request = new RestRequest("math");
+                request.AddParameter("operation", operation);
+                foreach (var input in inputs.Split('\n'))
+                {
+                    request.AddParameter("inputs", input.Trim());
+                }
+                result = (await client.Execute<string>(request)).Content;
+            }
 
             var output = new StringBuilder();
             output.AppendLine($"operation = {operation}");
